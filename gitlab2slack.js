0f4647a52aff3277918e44a7b23895123c1e81ac
Victor Xian Wang <victor.xianwang@gmail.com>
2015-09-21 20:07:09 -0400

    fix a bug where it only pushes the first commit when there's multiple commits

--- gitlab2slack.js
+++ gitlab2slack.js
@@ -3,7 +3,7 @@
   * author: Victor Wang (xw72@duke.edu)
   *
   * This is a autobot that catches gitlab push event webhook and generates a formatted message to post in designated slack group.
-  * 
+  *
   * To get started, simply replace the value in slackURL with your slack incoming webhook url.
   */
 
@@ -23,55 +23,62 @@ app.get(['/', '/msg.json'], function (req, res) {
 
 app.post('/msg.json', function (req, res) {
 
-  var author = req.body.commits[0].author.name;
   var branch = req.body.ref.split('/').pop();
   var repoName = "<" + req.body.repository.homepage + "|" + req.body.repository.git_ssh_url.split(':').pop() + ">";
-  var commitURL = req.body.commits[0].url;
-  var commitID = "<" + req.body.commits[0].url + "|" + req.body.checkout_sha.substring(0, 7) + ">";
-  var commitMsg = req.body.commits[0].message;
 
-
-  var toSlackMsg = {
-    username: "gitlab-bot",
-    "icon_emoji": ":ghost:",
-    "attachments": [
-      {
-        "fallback": author + " pushed to branch " + branch + " at " + repoName + ". Commit SHA: " + commitID,
-        "pretext": author + " pushed to branch " + branch + " at " + repoName + ". Commit SHA: " + commitID,
-        "color": "#00FFFF",
-        "fields": [
-          {
-            "title": "Commit: " + req.body.checkout_sha.substring(0, 7) + " by " + author,
-            "value": commitMsg,
-            "short": false
-          }
-        ]
-      }
-    ]
-
-  };
-
-  // TODO: implement first push in a branch
-  var firstInBranch = false;
-  if (req.body.before === '0000000000000000000000000000000000000000') {
-    firstInBranch = true;
-  }
+  var commtList = req.body.commits;
 
   var request = require('request');
 
-  var options = {
-    uri:slackURL,
-    method: 'POST',
-    json: toSlackMsg
-  };
+  commitList.forEach(function (elem) {
 
-  request.post(options, function (err, response, body) {
-    if (err) {
-      console.err(err);
+    var author = elem.author.name;
+    var commitURL = elem.url;
+    var commitID = "<" + elem.url + "|" + req.body.checkout_sha.substring(0, 7) + ">";
+    var commitMsg = elem.message;
+
+
+    var toSlackMsg = {
+      username: "gitlab-bot",
+      "icon_emoji": ":ghost:",
+      "attachments": [
+        {
+          "fallback": author + " pushed to branch " + branch + " at " + repoName + ". Commit SHA: " + commitID,
+          "pretext": author + " pushed to branch " + branch + " at " + repoName + ". Commit SHA: " + commitID,
+          "color": "#00FFFF",
+          "fields": [
+            {
+              "title": "Commit: " + req.body.checkout_sha.substring(0, 7) + " by " + author,
+              "value": commitMsg,
+              "short": false
+            }
+          ]
+        }
+      ]
+
+    };
+
+    // TODO: implement first push in a branch
+    var firstInBranch = false;
+    if (req.body.before === '0000000000000000000000000000000000000000') {
+      firstInBranch = true;
     }
-    else {
-      console.log(body);
-    }
+
+    var options = {
+      uri:slackURL,
+      method: 'POST',
+      json: toSlackMsg
+    };
+
+    request.post(options, function (err, response, body) {
+      if (err) {
+        console.err(err);
+      }
+      else {
+        console.log(body);
+      }
+    });
+
   });
 
   res.send("ok");
